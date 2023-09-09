7a80af0302d8a24e69022e0611cf3c4eaa2c8d3d
leeve1247 <69492686+leeve1247@users.noreply.github.com>
2023-09-09 20:38:15 +0900

    Add Scripts

---
+++ Assets/Scripts/CanvasScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasScript : MonoBehaviour
+{
+    public void OnClickButton()
+    {
+        Debug.Log("버튼을 클릭하였다");
+    }
+}
